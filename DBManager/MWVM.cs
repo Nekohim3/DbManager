0f01f6103fb4cf79dec4c758a1237b215d59a7a4
Neko <kuroneko@protonmail.ch>
2021-10-25 12:49:57 +0300

    Вроде работает

--- DBManager/MWVM.cs
+++ DBManager/MWVM.cs
@@ -1,20 +1,32 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Windows;
-
+using System.Windows.Threading;
 using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Prism.ViewModel;
 using Microsoft.SqlServer.Management.Sdk.Sfc;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace DBManager
 {
     public class MWVM : NotificationObject
     {
-        public  DelegateCommand                AddDatabaseCommand    { get; }
-        public  DelegateCommand                RemoveDatabaseCommand { get; }
-        private DataBase                       _selectedDatabase;
-        private ObservableCollection<DataBase> _databaseList;
+        public  DelegateCommand                   AddDatabaseCommand           { get; }
+        public  DelegateCommand                   RemoveDatabaseCommand        { get; }
+        public  DelegateCommand                   CreateBackupFromDbCommand    { get; }
+        public  DelegateCommand                   RestoreSelectedBackupCommand { get; }
+        public  DelegateCommand                   DeleteSelectedBackupCommand  { get; }
+        public  DelegateCommand                   AddBackupFromFileCommand     { get; }
+        public  DelegateCommand                   ChangeDataDirCommand               { get; }
+        private DataBase                          _selectedDatabase;
+        private ObservableCollection<DataBase>    _databaseList;
+        private BackupClass                       _selectedBackup;
+        private ObservableCollection<BackupClass> _backupList;
+        private Visibility                        _processVis;
+        private string                            _processText;
 
         public ObservableCollection<DataBase> DatabaseList
         {
@@ -28,13 +40,73 @@ namespace DBManager
             set
             {
                 _selectedDatabase = value;
+                if (_selectedDatabase != null)
+                {
+                    BackupList = new ObservableCollection<BackupClass>(SQLService.GetBackups(_selectedDatabase));
+                }
+
                 RaisePropertyChanged(() => SelectedDatabase);
+                RaiseCanExecChange();
+            }
+        }
+
+        public ObservableCollection<BackupClass> BackupList
+        {
+            get => _backupList;
+            set
+            {
+                _backupList = value;
+                RaisePropertyChanged(() => BackupList);
+                var usedDb = SQLService.GetUsedBackup(SelectedDatabase);
+                foreach (var item in _backupList)
+                    item.InUse = item.Name == usedDb;
+            }
+        }
+
+        public BackupClass SelectedBackup
+        {
+            get => _selectedBackup;
+            set
+            {
+                _selectedBackup = value;
+                RaisePropertyChanged(() => SelectedBackup);
+                RaiseCanExecChange();
+            }
+        }
+
+        public Visibility ProcessVis
+        {
+            get => _processVis;
+            set
+            {
+                _processVis = value;
+                RaisePropertyChanged(() => ProcessVis);
+            }
+        }
+
+        public string ProcessText
+        {
+            get => _processText;
+            set
+            {
+                _processText = value;
+                RaisePropertyChanged(() => ProcessText);
             }
         }
 
         public MWVM()
         {
-            g.Settings = Settings.Load() ?? new Settings();
+            ProcessVis                   = Visibility.Hidden;
+            AddDatabaseCommand           = new DelegateCommand(OnAddDatabase);
+            RemoveDatabaseCommand        = new DelegateCommand(OnRemoveDatabase);
+            CreateBackupFromDbCommand    = new DelegateCommand(OnCreateBackupFromDb,    () => SelectedDatabase != null);
+            RestoreSelectedBackupCommand = new DelegateCommand(OnRestoreSelectedBackup, () => SelectedBackup   != null);
+            AddBackupFromFileCommand     = new DelegateCommand(OnAddBackupFromFile,     () => SelectedDatabase != null);
+            DeleteSelectedBackupCommand  = new DelegateCommand(OnRemoveBackup,          () => SelectedBackup   != null);
+            ChangeDataDirCommand         = new DelegateCommand(ChangeDataDir);
+
+
+            g.Settings                = Settings.Load() ?? new Settings();
 
             while (string.IsNullOrEmpty(g.Settings.DirForDbData))
             {
@@ -52,15 +124,20 @@ namespace DBManager
 
             DatabaseList          = new ObservableCollection<DataBase>(g.Settings.DbList);
             SelectedDatabase      = DatabaseList.Count > 0 ? DatabaseList.FirstOrDefault() : null;
-            
-            AddDatabaseCommand    = new DelegateCommand(OnAddDatabase);
-            RemoveDatabaseCommand = new DelegateCommand(OnRemoveDatabase);
+        }
+
+        private void RaiseCanExecChange()
+        {
+            CreateBackupFromDbCommand.RaiseCanExecuteChanged();
+            RestoreSelectedBackupCommand.RaiseCanExecuteChanged();
+            AddBackupFromFileCommand.RaiseCanExecuteChanged();
+            DeleteSelectedBackupCommand.RaiseCanExecuteChanged();
         }
 
         private void OnAddDatabase()
         {
-            var f  = new DBManagerForm();
-            var vm = new DBManagerViewModel(f.Close);
+            var f  = new DBAddForm();
+            var vm = new DBAddViewModel(f.Close);
             f.DataContext = vm;
             f.ShowDialog();
 
@@ -69,10 +146,6 @@ namespace DBManager
                 g.Settings.AddDb(new DataBase() { Instance = vm.SelectedInstance, Name = vm.SelectedDatabase });
                 DatabaseList     = new ObservableCollection<DataBase>(g.Settings.DbList);
                 SelectedDatabase = DatabaseList.Count > 0 ? DatabaseList.FirstOrDefault() : null;
-
-                if (SelectedDatabase != null)
-                    if (!Directory.Exists($"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}"))
-                        Directory.CreateDirectory($"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}");
             }
         }
 
@@ -86,6 +159,197 @@ namespace DBManager
             DatabaseList     = new ObservableCollection<DataBase>(g.Settings.DbList);
             SelectedDatabase = DatabaseList.Count > 0 ? DatabaseList.FirstOrDefault() : null;
         }
-        
+
+        private void OnCreateBackupFromDb()
+        {
+            var f  = new CreateBackupForm();
+            var vm = new CreateBackupViewModel(f.Close, SelectedDatabase);
+            f.DataContext = vm;
+            f.ShowDialog();
+            if (!vm.Cancel)
+            {
+                ProcessText = "Создание бэкапа, плис вэит...";
+                ProcessVis  = Visibility.Visible;
+                ThreadPool.QueueUserWorkItem(x =>
+                                             {
+                                                 Directory
+                                                    .CreateDirectory($"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{vm.Name}");
+                                                 if(File.Exists($"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{vm.Name}\\{vm.Name}.bak"))
+                                                     File.Delete($"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{vm.Name}\\{vm.Name}.bak");
+                                                 var bak = new BackupClass() { CreateTime = DateTime.Now, Name = vm.Name, Desc = vm.Desc };
+                                                 SQLService.BackupDatabase($"{g.CompName}\\{SelectedDatabase.Instance}", SelectedDatabase.Name,
+                                                                           $"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{vm.Name}\\{vm.Name}.bak",
+                                                                           this);
+                                                 bak.Save($"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{vm.Name}\\data.xml");
+                                                 BackupList = new ObservableCollection<BackupClass>(SQLService.GetBackups(_selectedDatabase));
+                                                 ProcessVis = Visibility.Hidden;
+                                             });
+            }
+        }
+
+        private void OnRestoreSelectedBackup()
+        {
+            ProcessText = "Восстановление бэкапа, плис вэит...";
+            ProcessVis  = Visibility.Visible;
+            ThreadPool.QueueUserWorkItem(x =>
+                                         {
+                                             SQLService.RestoreDatabase($"{g.CompName}\\{SelectedDatabase.Instance}", SelectedDatabase.Name,
+                                                                        $"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{SelectedBackup.Name}\\{SelectedBackup.Name}.bak",
+                                                                        $"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{SelectedBackup.Name}",
+                                                                        this);
+
+                                             ProcessVis = Visibility.Hidden;
+                                         });
+        }
+
+        private void OnAddBackupFromFile()
+        {
+            var dialog = new CommonOpenFileDialog
+                         {
+                             IsFolderPicker = false,
+                             Filters = { new CommonFileDialogFilter("DB BAK", "*.bak") }
+                         };
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                if (File.Exists(dialog.FileName))
+                {
+                    var f  = new CreateBackupForm();
+                    var vm = new CreateBackupViewModel(f.Close, SelectedDatabase, true)
+                             {
+                                 Name     = Path.GetFileNameWithoutExtension(dialog.FileName)
+                             };
+                    f.DataContext = vm;
+                    f.ShowDialog();
+                    if (!vm.Cancel)
+                    {
+                        if (g.Settings.DirForDbData[0] != dialog.FileName[0])
+                        {
+                            //разные диски
+
+                            ProcessText = "Копирование бэкапа, плис вэит...";
+                            ProcessVis = Visibility.Visible;
+                            ThreadPool.QueueUserWorkItem(x =>
+                            {
+                                Directory
+                                   .CreateDirectory($"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{vm.Name}");
+                                if (
+                                    File
+                                       .Exists($"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{vm.Name}\\{vm.Name}.bak"))
+                                    File
+                                       .Delete($"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{vm.Name}\\{vm.Name}.bak");
+                                var bak = new BackupClass()
+                                { CreateTime = DateTime.Now, Name = vm.Name, Desc = vm.Desc };
+                                File.Copy(dialog.FileName,
+                                          $"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{vm.Name}\\{vm.Name}.bak");
+                                bak.Save($"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{vm.Name}\\data.xml");
+                                BackupList =
+                                    new ObservableCollection<BackupClass>(SQLService.GetBackups(_selectedDatabase));
+                                ProcessVis = Visibility.Hidden;
+                            });
+
+                        }
+                        else
+                        {
+                            ProcessText = vm.MoveIsChecked ? "Перемещение бэкапа, плис вэит..." : "Копирование бэкапа, плис вэит...";
+
+                            ProcessVis  = Visibility.Visible;
+                            ThreadPool.QueueUserWorkItem(x =>
+                                                         {
+                                                             Directory
+                                                                .CreateDirectory($"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{vm.Name}");
+                                                             if (
+                                                                 File
+                                                                    .Exists($"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{vm.Name}\\{vm.Name}.bak"))
+                                                                 File
+                                                                    .Delete($"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{vm.Name}\\{vm.Name}.bak");
+                                                             var bak = new BackupClass()
+                                                                       { CreateTime = DateTime.Now, Name = vm.Name, Desc = vm.Desc };
+                                                             if (vm.MoveIsChecked)
+                                                                 File.Move(dialog.FileName,
+                                                                           $"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{vm.Name}\\{vm.Name}.bak");
+                                                             if(vm.CopyIsChecked)
+                                                                 File.Copy(dialog.FileName,
+                                                                           $"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{vm.Name}\\{vm.Name}.bak");
+                                                             bak.Save($"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{vm.Name}\\data.xml");
+                                                             BackupList =
+                                                                 new ObservableCollection<BackupClass>(SQLService.GetBackups(_selectedDatabase));
+                                                             ProcessVis = Visibility.Hidden;
+                                                         });
+                        }
+                    }
+                }
+            }
+        }
+
+        private void OnRemoveBackup()
+        {
+            ProcessText = "Удаление бэкапа, плис вэит...";
+            ProcessVis  = Visibility.Visible;
+            ThreadPool.QueueUserWorkItem(x =>
+                                         {
+                                             if (File.Exists($"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{SelectedBackup.Name}\\{SelectedBackup.Name}.mdf"))
+                                             {
+                                                 SQLService.RemoveDatabase($"{g.CompName}\\{SelectedDatabase.Instance}", SelectedDatabase.Name);
+                                                 //MessageBox.Show("Впадлу");
+                                                 //return;
+                                             }
+
+                                             if (MessageBox.Show("Точно", "", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No) ==
+                                                 MessageBoxResult.No) return;
+                                             foreach (var item in
+                                                 Directory
+                                                    .GetFiles($"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{SelectedBackup.Name}\\"))
+                                                 File.Delete(item);
+                                             Directory
+                                                .Delete($"{g.Settings.DirForDbData}\\{SelectedDatabase.Instance}_{SelectedDatabase.Name}\\{SelectedBackup.Name}",
+                                                        true);
+
+                                             BackupList =
+                                                 new ObservableCollection<BackupClass>(SQLService.GetBackups(_selectedDatabase));
+                                             ProcessVis = Visibility.Hidden;
+                                         });
+        }
+
+        private void ChangeDataDir()
+        {
+            var oldDir = g.Settings.DirForDbData;
+
+            var f  = new SelectDirForm();
+            var vm = new SelectDirViewModel(f.Close);
+            f.DataContext = vm;
+            f.ShowDialog();
+            if (!Directory.Exists(vm.Dir)) return;
+
+            g.Settings.DirForDbData = vm.Dir;
+            if (Directory.Exists(g.Settings.DirForDbData))
+                g.Settings.Save();
+
+            //if (oldDir[0] != g.Settings.DirForDbData[0])
+            //{
+            //    CopyFilesRecursively(oldDir, g.Settings.DirForDbData);
+            //}
+            //else
+            //{
+            //    Directory.C(oldDir, g.Settings.DirForDbData);
+            //}
+
+        }
+
+        private static void CopyFilesRecursively(string sourcePath, string targetPath)
+        {
+            //Now Create all of the directories
+            foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+            {
+                Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
+            }
+
+            //Copy all the files & Replaces any files with the same name
+            foreach (string newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+            {
+                File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
+            }
+        }
+
     }
 }
--- DBManager/SQLService.cs
+++ DBManager/SQLService.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-
+using System.Xml.Serialization;
 using Microsoft.SqlServer.Management.Common;
 using Microsoft.SqlServer.Management.Smo;
 using Microsoft.Win32;
@@ -22,47 +22,44 @@ namespace DBManager
             }  
         }
 
-        public static void RestoreDatabase(string serverName, string databaseName, string bakFilePath, string dataDirPath)
+        public static void RestoreDatabase(string serverName, string databaseName, string bakFilePath, string dataDirPath, MWVM vm)
         {
 
             var conn = new ServerConnection
             {
-                    ServerInstance = serverName
+                    ServerInstance   = serverName,
+                    StatementTimeout = int.MaxValue
             };
-
             var srv  = new Server(conn);
             var path = Path.Combine(g.Settings.DirForDbData, databaseName);
 
             try
             {
                 var res = new Restore();
-
                 res.Devices.AddDevice(bakFilePath, DeviceType.File);
 
-                //var MDF = res.ReadFileList(srv).Rows[0][1].ToString();
-                var DataFile = new RelocateFile
-                {
-                        LogicalFileName  = res.ReadFileList(srv).Rows[0][0].ToString(),
-                        PhysicalFileName = $"{path}\\"
-                };
+                res.RelocateFiles.Add(new RelocateFile
+                                      {
+                                          LogicalFileName  = res.ReadFileList(srv).Rows[0][0].ToString(),
+                                          PhysicalFileName = $"{dataDirPath}\\{Path.GetFileNameWithoutExtension(bakFilePath)}.mdf"
+                                      });
+                res.RelocateFiles.Add(new RelocateFile
+                                                {
+                                                    LogicalFileName  = res.ReadFileList(srv).Rows[1][0].ToString(),
+                                                    PhysicalFileName = $"{dataDirPath}\\{Path.GetFileNameWithoutExtension(bakFilePath)}.ldf"
+                                                });
+                res.RelocateFiles.Add(new RelocateFile
+                                      {
+                                          LogicalFileName  = res.ReadFileList(srv).Rows[2][0].ToString(),
+                                          PhysicalFileName = $"{dataDirPath}\\Filestore"
+                                      });
 
-                var LogFile = new RelocateFile();
-                var       LDF     = res.ReadFileList(srv).Rows[1][1].ToString();
-                LogFile.LogicalFileName  = res.ReadFileList(srv).Rows[1][0].ToString();
-                LogFile.PhysicalFileName = @"C:\WORK\qwe.ldf";
-
-                var FileStore = new RelocateFile();
-                var       FS     = res.ReadFileList(srv).Rows[2][1].ToString();
-                FileStore.LogicalFileName  = res.ReadFileList(srv).Rows[2][0].ToString();
-                FileStore.PhysicalFileName = @"C:\WORK\FileStore";
-
-                res.RelocateFiles.Add(DataFile);
-                res.RelocateFiles.Add(LogFile);
-                res.RelocateFiles.Add(FileStore);
-
-                res.Database = databaseName;
-                res.NoRecovery = false;
-                res.ReplaceDatabase = true;
+                
+                res.Database                    =  databaseName;
+                res.NoRecovery                  =  false;
+                res.ReplaceDatabase             =  true;
+                res.PercentCompleteNotification =  5;
+                res.PercentComplete             += (sender, args) => { vm.ProcessText = $"Восстановление бэкапа, плис вэит...{args.Percent}%"; };
                 res.SqlRestore(srv);
                 conn.Disconnect();
             }
@@ -90,10 +87,13 @@ namespace DBManager
             return lst;
         }
         
-        public static void BackupDatabase(string serverName, string databaseName, string filePath)
+        public static bool BackupDatabase(string serverName, string databaseName, string filePath, MWVM vm)
         {
-            var conn                = new ServerConnection();
-            conn.ServerInstance = serverName;
+            var conn = new ServerConnection
+                       {
+                           ServerInstance   = serverName,
+                           StatementTimeout = int.MaxValue
+                       };
             var srv                 = new Server(conn);
 
             try
@@ -106,6 +106,8 @@ namespace DBManager
                 bkp.Devices.AddDevice(filePath, DeviceType.File);
                 bkp.Incremental = false;
 
+                bkp.PercentCompleteNotification = 5;
+                bkp.PercentComplete += (sender, args) => { vm.ProcessText = $"Создание бэкапа, плис вэит...{args.Percent}%"; };
                 bkp.SqlBackup(srv);
 
                 conn.Disconnect();
@@ -119,6 +121,102 @@ namespace DBManager
             {
                 throw new IOException(ex.Message, ex.InnerException);
             }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message, e.InnerException);
+            }
+
+            return true;
+        }
+
+        public static List<BackupClass> GetBackups(DataBase db)
+        {
+            if (!Directory.Exists($"{g.Settings.DirForDbData}\\{db.Instance}_{db.Name}"))
+                Directory.CreateDirectory($"{g.Settings.DirForDbData}\\{db.Instance}_{db.Name}");
+
+            var lst = new List<BackupClass>();
+
+            foreach (var item in Directory.GetDirectories($"{g.Settings.DirForDbData}\\{db.Instance}_{db.Name}"))
+            {
+                if (!File.Exists($"{item}\\data.xml")) continue;
+                var formatter = new XmlSerializer(typeof(BackupClass));
+
+                using (var fs = new FileStream($"{item}\\data.xml", FileMode.Open))
+                    lst.Add((BackupClass)formatter.Deserialize(fs));
+            }
+
+            return lst;
+        }
+
+        public static bool RemoveDatabase(string serverName, string dbName)
+        {
+            var conn = new ServerConnection { ServerInstance = serverName };
+
+            var srv = new Server(conn);
+
+            try
+            {
+                foreach (Database item in srv.Databases)
+                {
+                    if (item.Name != dbName) continue;
+                    item.Drop();
+                    return true;
+                }
+            }
+            catch (SmoException ex)
+            {
+                throw new SmoException(ex.Message, ex.InnerException);
+            }
+            catch (IOException ex)
+            {
+                throw new IOException(ex.Message, ex.InnerException);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+
+            return true;
+        }
+
+        public static string GetUsedBackupNameFromDb(string serverName, string dbName)
+        {
+            var conn = new ServerConnection { ServerInstance = serverName };
+
+            var srv = new Server(conn); 
+            try
+            {
+                foreach (Database item in srv.Databases)
+                {
+                    if (item.Name != dbName) continue;
+                    return Path.GetFileNameWithoutExtension(item.PrimaryFilePath);
+                }
+            }
+            catch (SmoException ex)
+            {
+                throw new SmoException(ex.Message, ex.InnerException);
+            }
+            catch (IOException ex)
+            {
+                throw new IOException(ex.Message, ex.InnerException);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+
+            return "";
+        }
+
+        public static string GetUsedBackup(DataBase db)
+        {
+            foreach (var item in Directory.GetDirectories($"{g.Settings.DirForDbData}\\{db.Instance}_{db.Name}"))
+            {
+                var files = Directory.GetFiles(item);
+                if (files.Length > 2) return Path.GetFileNameWithoutExtension(files.First(x => x.EndsWith(".mdf")));
+            }
+
+            return "";
         }
     }
 }
